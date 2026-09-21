06ab711a81efc7f4a12d6494fc3ae44950245f5f
Ta Hoang Luong <tahoangluong2005@example.com>
2026-09-21 14:18:20 +0700

    Initial commit for lab1

---
+++ src/components/Banner.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Container, Navbar, Nav, Card, Button, Row, Col, Carousel } from 'react-bootstrap';
+
+ <Carousel className="mb-4">
+        <Carousel.Item style={{ maxHeight: '400px' }}>
+          <img
+            className="d-block w-100"
+            src="D:\FER202\Images\banner1.jpg"
+            src="D:\FER202\Images\banner2.jpg"
+            src="D:\FER202\Images\banner3.jpg"
+            alt="First slide"
+            style={{ height: '400px', objectFit: 'cover' }}
+          />
+          <Carousel.Caption>
+            <h3>FASHION IMAGE</h3>
+            <p>FASHION COLLECTION 2026</p>
+            <p>Discover latest fashion..</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+ import React from 'react';
+import { Container, Navbar, Nav, Card, Button, Row, Col, Carousel } from 'react-bootstrap';
+
+ <Navbar bg="dark" variant="dark" expand="lg" className="px-4">
+        <Navbar.Brand href="#home" className="fw-bold text-warning">Pizza House</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#SHOPFASHION">SHOP FASHION</Nav.Link>
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#Products">Products</Nav.Link>
+            <Nav.Link href="#Men">Men</Nav.Link>
+            <Nav.Link href="#Women">Women</Nav.Link>
+            <Nav.Link href="#Contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
