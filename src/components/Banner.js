d70a3d3c0e4a1826a3c82f8c2048de9413dd6758
luonghoang205 <tahoangluong2005@gmail.com>
2026-09-21 14:44:42 +0700

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,20 +1,39 @@
 import React from 'react';
-import { Container, Navbar, Nav, Card, Button, Row, Col, Carousel } from 'react-bootstrap';
+import { Carousel, Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
- <Carousel className="mb-4">
-        <Carousel.Item style={{ maxHeight: '400px' }}>
-          <img
-            className="d-block w-100"
-            src="D:\FER202\Images\banner1.jpg"
-            src="D:\FER202\Images\banner2.jpg"
-            src="D:\FER202\Images\banner3.jpg"
-            alt="First slide"
-            style={{ height: '400px', objectFit: 'cover' }}
-          />
-          <Carousel.Caption>
-            <h3>FASHION IMAGE</h3>
-            <p>FASHION COLLECTION 2026</p>
-            <p>Discover latest fashion..</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
+function Banner() {
+    return (
+        <Container>
+            <h1 className='text-center'>PROMOTIONAL BANNERS</h1>
+            <Carousel className="position-relative" data-bs-ride="carousel">
+                <Carousel.Item>
+                    <img src="/Images/banner1.jpg" className="d-block w-100" style={{ height: '450px', objectFit: 'cover' }} alt="Pizza 1" />
+                    <Carousel.Caption>
+                        <h5>FASHION COLLECTION 2026</h5>
+                        <p>Discover latest fashion trends for 2026</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+
+                <Carousel.Item>
+                    <img src="/Images/banner2.jpg" className="d-block w-100" style={{ height: '450px', objectFit: 'cover' }} alt="Pizza 2" />
+                    <Carousel.Caption>
+                        <h5>SUMMER SALE UP TO 50%</h5>
+                        <p>Enjoy special discounts on selected products</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+
+                <Carousel.Item>
+                    <img src="/Images/banner3.jpg" className="d-block w-100" style={{ height: '450px', objectFit: 'cover' }} alt="Pizza 3" />
+                    <Carousel.Caption>
+                        <h5>NEW ARRIVALS</h5>
+                        <p>Explore our newest clothing collection</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+        </Container>
+
+    );
+}
+
+export default Banner;
